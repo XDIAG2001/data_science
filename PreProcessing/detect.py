c526f013a79b14df2bf384a9e72ee86ee1989784
XDIAG2001 <achbabmahdi120@gmail.com>
2023-10-14 15:25:28 +0100

    eza

---
+++ PreProcessing/detect.py
@@ -0,0 +1,19 @@
+import pyautogui
+
+while True:
+    pyautogui.press('space')                 
+   
+      
+"""while True:
+    button7location = pyautogui.locateOnScreen('Capture.PNG',confidence=0.8)
+    if button7location is None:
+        print("not found")
+        
+    else:
+        print("found it")
+        time.sleep(2)
+        pyautogui.press('space')
+        time.sleep(1) 
+        pyautogui.press('f1')
+        pyautogui.press('space')
+    time.sleep(0.5)"""
